248a920fea26ff183ce4cc89f6d79805894b7b02
Thomas Massmann <thomas.massmann@it-spir.it>
2013-07-30 15:49:44 +0200

    Updated package. Added more docs.

--- README.rst
+++
@@ -1,9 +0,0 @@
-Introduction
-============
-
-This package integrate bootstrap datepicker with fanstatic.
-
-http://www.eyecon.ro/bootstrap-datepicker/
-
-    from js.bootstrapdatepicker import bootstrapdatepicker
-    bootstrapdatepicker.need()
--- setup.py
+++ setup.py
@@ -1,30 +1,43 @@
 from setuptools import setup, find_packages
-import sys, os
+import os
 
 version = '1.0'
 
-setup(name='js.bootstrapdatepicker',
-      version=version,
-      description="bootstrap date picker fanstatic integration",
-      long_description=open('README.rst').read(),
-      classifiers=[],
-      keywords='fanstatic bootstrap date widget',
-      author='Nathan Van Gheem',
-      author_email='vangheem@gmail.com',
-      url='',
-      license='GPL',
-      packages=find_packages(),
-      namespace_packages=['js'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-            'fanstatic',
-            'js.jquery',
-            'setuptools'
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      [fanstatic.libraries]
-      bootstrap = js.bootstrapdatepicker:library
-      """,
-      )
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    read('js', 'bootstrapdatepicker', 'test_bootstrapdatepicker.txt')
+    + '\n' +
+    read('CHANGES.txt'))
+
+setup(
+    name='js.bootstrapdatepicker',
+    version=version,
+    description="Fanstatic packaging of bootstrap-datepicker.js.",
+    long_description=long_description,
+    classifiers=[],
+    keywords='fanstatic bootstrap date widget',
+    author='Nathan Van Gheem',
+    author_email='vangheem@gmail.com',
+    url='https://github.com/vangheem/js.bootstrapdatepicker',
+    download_url='https://pypi.python.org/pypi/js.bootstrapdatepicker',
+    license='GPL',
+    packages=find_packages(),
+    namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'fanstatic',
+        'js.bootstrap',
+        'setuptools'
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    [fanstatic.libraries]
+    bootstrap = js.bootstrapdatepicker:library
+    """,
+)
