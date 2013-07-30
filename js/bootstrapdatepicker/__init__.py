556588d89dc6a20d7daa5042faa4b9583a7906d3
Thomas Massmann <thomas.massmann@it-spir.it>
2013-07-30 14:22:19 +0200

    Moved resources out of subdirectory for better bundling.

--- js/bootstrapdatepicker/__init__.py
+++ js/bootstrapdatepicker/__init__.py
@@ -1,12 +1,21 @@
+# -*- coding: utf-8 -*-
+
 from fanstatic import Library, Resource, Group
 from js.bootstrap import bootstrap_css, bootstrap_js
 
 library = Library('bootstrapdatepicker', 'resources')
 
-bootstrapdatepicker_css = Resource(library, 'css/bootstrap-datepicker.css',
-                         depends=[bootstrap_css])
+bootstrapdatepicker_css = Resource(
+    library, 'bootstrap-datepicker.css',
+    depends=[bootstrap_css],
+)
 
-bootstrapdatepicker_js = Resource(library, 'js/bootstrap-datepicker.js',
-                        depends=[bootstrap_js])
+bootstrapdatepicker_js = Resource(
+    library, 'bootstrap-datepicker.js',
+    depends=[bootstrap_js],
+)
 
-bootstrapdatepicker = Group([bootstrapdatepicker_css, bootstrapdatepicker_js])
+bootstrapdatepicker = Group([
+    bootstrapdatepicker_css,
+    bootstrapdatepicker_js,
+])
